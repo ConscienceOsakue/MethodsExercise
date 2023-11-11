02b8e3605115751e3aed262cfef4c7007881c055
conscience osakue <consciencenosa@gmail.com>
2023-11-11 16:36:13 -0600

    Begin user questions

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace MethodsExercise
+﻿using System.Drawing;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //-------------- Exercise 1 ---------------
+            //Nmae: Micheal 
+            //Favorite Color: Blue
+            //Favorite Animal: Walrus
+            //Favorite Band:  The Beatles
+ 
+            Console.WriteLine("Hello new student, what is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"{userName} is an awesome name! Tell the class your favorite color.");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"Nice, {color} is a beautiful color. The class would love to know the name of your favorite Animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"{animal} is a unique choice. Lastly let the class your favorite band's name?");
+            var band = Console.ReadLine();
         }
     }
 }
